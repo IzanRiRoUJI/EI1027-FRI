ae149dcdeef24f15f5348cc26b9824dc673b10bc
Tomate77 <aguerofeli21@gmail.com>
2022-03-05 16:56:24 +0100

    sql serial
    skill con id serial

--- SSUji/inserts.sql
+++ SSUji/inserts.sql
@@ -25,20 +25,23 @@ VALUES ('Aleman','Pues una clase normal la verdad, unos pocos de der Abend poraq
 INSERT INTO Skill (name, description, level)
 VALUES ('Fuegos','Pues una clase normal la verdad, de fuego', 'beginner');
 
+INSERT INTO Skill (name, description, level)
+VALUES ('Fuegos','Pues una clase normal la verdad, de fuego', 'expert');
+
 <-- Skill -->
 
 
 <-- Offer -->
 
-INSERT INTO Offer VALUES ('Ayuda en Matematicas 2' , '1234', 'Matematicas', 'beginner', 'necesito ayuda urgente', TO_DATE('04/03/2022', 'DD/MM/YYYY'), TO_DATE('14/03/2022', 'DD/MM/YYYY'));
+INSERT INTO Offer VALUES ('Ayuda en Matematicas 2' , '1234', 1 , 'necesito ayuda urgente', TO_DATE('04/03/2022', 'DD/MM/YYYY'), TO_DATE('14/03/2022', 'DD/MM/YYYY'));
 
 <-- Offer -->
 
 <--- Request -->
-INSERT INTO Request VALUES ('Ayuda en Matematicas 2' , '5678', 'Matematicas', 'beginner', 'doy ayuda', TO_DATE('04/03/2022', 'DD/MM/YYYY'), TO_DATE('14/03/2022', 'DD/MM/YYYY'));
+INSERT INTO Request VALUES ('Ayuda en Matematicas 2' , '5678', 1, 'doy ayuda', TO_DATE('04/03/2022', 'DD/MM/YYYY'), TO_DATE('14/03/2022', 'DD/MM/YYYY'));
 
 <-- Collaboration -->
 
-INSERT INTO Collaboration VALUES ('1234','5678','Matematicas','beginner','Mi casa','notStarted',0,0,TO_DATE('04/03/2022','DD/MM/YYYY'),TO_DATE('07/03/2022','DD/MM/YYYY'));
+INSERT INTO Collaboration VALUES ('1234','5678', 1 ,'Mi casa','notStarted',0,0,TO_DATE('04/03/2022','DD/MM/YYYY'),TO_DATE('07/03/2022','DD/MM/YYYY'));
 
 <-- Collaboration -->
--- SSUji/instruccionesBases.sql
+++ SSUji/instruccionesBases.sql
@@ -10,66 +10,68 @@ CREATE TABLE Student (
     CONSTRAINT calt_student UNIQUE (email)
 );
 
+CREATE TABLE Skill (
+    id			SERIAL,
+    name        VARCHAR(50),
+    description VARCHAR(100),
+    level       VARCHAR(50),
+    active      BOOLEAN DEFAULT TRUE,
+
+    CONSTRAINT cp_skill PRIMARY KEY (id),
+
+    CONSTRAINT calt_skill UNIQUE (name, level),
+    CONSTRAINT ri_skill_level CHECK (level in ('beginner', 'intermediate', 'expert'))
+);
+
 CREATE TABLE Offer (
     name        VARCHAR(50),
     dniOffer    VARCHAR(10),
-    skillName   VARCHAR(50),
-    skillLevel  VARCHAR(50),
+    skillId     SERIAL,
     description VARCHAR(100),
     startDate   DATE,
     endDate     DATE,
-    CONSTRAINT cp_offer PRIMARY KEY (dniOffer, skillName, skillLevel),
+    CONSTRAINT cp_offer PRIMARY KEY (dniOffer, skillId),
     CONSTRAINT ca_offer_student FOREIGN KEY (dniOffer) REFERENCES Student(dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_offer_skill FOREIGN KEY (skillId) REFERENCES Skill(id) ON DELETE RESTRICT ON UPDATE CASCADE,
 
-    CONSTRAINT ca_offer_skill FOREIGN KEY (skillName) REFERENCES Skill(name) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT ca_offer_skill_level FOREIGN KEY (skillLevel) REFERENCES Skill(level) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT ri_offer_endDate CHECK (endDate >= startDate)
 );
 
 CREATE TABLE Request (
     name        VARCHAR(50),
     dniRequest  VARCHAR(10),
-    skillName   VARCHAR(50),
-    skillLevel  VARCHAR(50),
+    skillId     SERIAL,
     description VARCHAR(100),
     startDate   DATE,
     endDate     DATE,
-    CONSTRAINT cp_request PRIMARY KEY (dniRequest, skillName, skillLevel),
-    CONSTRAINT ca_request_student FOREIGN KEY (dnirequest) REFERENCES Student(dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT cp_request PRIMARY KEY (dniRequest, skillId),
+    CONSTRAINT ca_request_student FOREIGN KEY (dniRequest) REFERENCES Student(dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_request_skill FOREIGN KEY (skillId) REFERENCES Skill(id) ON DELETE RESTRICT ON UPDATE CASCADE,
 
-    CONSTRAINT ca_request_skill_skill FOREIGN KEY (skillName) REFERENCES Skill(name) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT ca_request_skill_level FOREIGN KEY (skillLevel) REFERENCES Skill(skill) ON DELETE RESTRICT ON UPDATE CASCADE
     CONSTRAINT ri_request_endDate CHECK (endDate >= startDate)
 );
 
 CREATE TABLE Collaboration (
     dniOffer    VARCHAR(10),
     dniRequest  VARCHAR(10),
-    skillName   VARCHAR(50),
-    skillLevel  VARCHAR(50),
+    skillId     SERIAL,
     place       VARCHAR(100),
-    state       VARCHAR(50),
+    state       VARCHAR(50) DEFAULT 'notStarted',
     score       INTEGER,
     hours       FLOAT,
     startDate   DATE,
     endDate     DATE,
-    CONSTRAINT cp_collaboration PRIMARY KEY (dniOffer, dniRequest, skillName, skillLevel, state),
-    CONSTRAINT ca_collaboration_offer FOREIGN KEY (dniOffer) REFERENCES Offer(dniOffer) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT ca_collaboration_request FOREIGN KEY (dnirequest) REFERENCES Request(dniRequest) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT cp_collaboration PRIMARY KEY (dniOffer, dniRequest, skillId, state),
 
-    CONSTRAINT ca_collaboration_skill_skill FOREIGN KEY (skillName) REFERENCES Skill(skill) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT ca_collaboration_skill_level FOREIGN KEY (skillLevel) REFERENCES Skill(level) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_collaboration_offer FOREIGN KEY (dniOffer, skillId) REFERENCES Offer(dniOffer, skillId) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_collaboration_request FOREIGN KEY (dniRequest, skillId) REFERENCES Request(dniRequest, skillId) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_collaboration_skill FOREIGN KEY (skillId) REFERENCES Skill(id) ON DELETE RESTRICT ON UPDATE CASCADE,
 
     CONSTRAINT ri_collaboration_score CHECK (score BETWEEN 0 AND 5),
     CONSTRAINT ri_collaboration_state CHECK (state in ('notStarted', 'inProgress', 'finished')),
     CONSTRAINT ri_collaboration_endDate CHECK (endDate >= startDate)
+    -- No sea el mismo estudante
+    --  CONSTRAINT ri_collaboration_sameSkill CHECK (offer.skillId == request.skillId), -- Mismas skills
+    -- Las fechas dentro del mismo rango
 );
 
-CREATE TABLE Skill (
-    name        VARCHAR(50),
-    description VARCHAR(100),
-    level       VARCHAR(50),
-    active      BOOLEAN DEFAULT TRUE,
-    CONSTRAINT cp_skill PRIMARY KEY (name, level),
-    CONSTRAINT ri_skill_level CHECK (level in ('beginner', 'intermediate', 'expert'))
-);
