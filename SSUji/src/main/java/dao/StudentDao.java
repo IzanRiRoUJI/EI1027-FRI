e0621e107657ee1b0bd454d6a6278f207af9a63e
al394830 <al394830@uji.es>
2022-03-05 17:38:07 +0100

    Creacion DAO de Student

---
+++ SSUji/src/main/java/dao/StudentDao.java
@@ -0,0 +1,60 @@
+package dao;
+
+import model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class StudentDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void addStudent(Student student) {
+        jdbcTemplate.update("INSERT INTO Student VALUES (?,?,?,?,?)",
+                student.getDni(), student.getName(), student.getEmail(),
+                student.getDegree(), student.getBalance(), student.isSKP());
+    }
+
+    public void deleteStudent(Student student) {
+        jdbcTemplate.update("DELETE FROM Student WHERE dni = ?",
+                student.getDni());
+    }
+
+    public void updateStudent(Student student) {
+        jdbcTemplate.update("UPDATE Student " +
+                "SET name=?, email=?, degree=?, balance=?, isSKP=?" +
+                "WHERE dni=?",
+                student.getName(), student.getEmail(), student.getDegree(),
+                student.getBalance(), student.isSKP(), student.getDni());
+    }
+
+    public Student getStudent(String dniStudent) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM Student WHERE dni=?",
+                    new StudentRowMapper(), dniStudent);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public List<Student> getStudents() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM Student", new StudentRowMapper());
+        }
+        catch(EmptyResultDataAccessException e) {
+            return new ArrayList<Student>();
+        }
+    }
+
+}
