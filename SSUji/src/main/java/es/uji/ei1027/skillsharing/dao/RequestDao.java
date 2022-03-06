62a23e5c7bfcad6ebf1ffc26eab167a229ae080b
IzanRiRoUJI <79511389+IzanRiRoUJI@users.noreply.github.com>
2022-03-06 22:13:29 +0100

    DaoReq

---
+++ SSUji/src/main/java/es/uji/ei1027/skillsharing/dao/RequestDao.java
@@ -0,0 +1,64 @@
+package es.uji.ei1027.skillsharing.dao;
+
+import es.uji.ei1027.skillsharing.model.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RequestDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void addRequest(Request request) {
+        jdbcTemplate.update("INSERT INTO Request VALUES (?,?,?,?,?,?)",
+                request.getName(), request.getDniRequest(), request.getSkillId(),
+                request.getDescription(), request.getStartDate(), request.getEndDate());
+    }
+
+    public void deleteRequest(Request request) {
+        jdbcTemplate.update("DELETE FROM Request WHERE dniRequest = ? AND skillId = ?",
+                request.getDniRequest(), request.getSkillId());
+    }
+
+    public void deleteRequest(String dniRequest) {
+        jdbcTemplate.update("DELETE FROM Request WHERE dniRequest = ?",
+                dniRequest);
+    }
+
+    public void updateRequest(Request request) {
+        jdbcTemplate.update("UPDATE Request " +
+                        "SET name=?, description=?, starDate=?, endDate=?" +
+                        "WHERE dniRequest=? AND skillId=?",
+                request.getName(), request.getDescription(), request.getStartDate(),
+                request.getEndDate(), request.getDniRequest(), request.getSkillId());
+    }
+
+    public Request getRequest(String dniRequest) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM Request WHERE dniRequest=?",
+                    new RequestRowMapper(), dniRequest);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public List<Request> getRequests() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM Request", new RequestRowMapper());
+        }
+        catch(EmptyResultDataAccessException e) {
+            return new ArrayList<Request>();
+        }
+    }
+}
