22cd744a635b561979dcda1475e7f5a6f77516bf
al394830 <al394830@uji.es>
2022-04-02 11:25:21 +0200

    Corrección CollaborationController

--- SSUji/src/main/java/es/uji/ei1027/skillsharing/controller/CollaborationController.java
+++ SSUji/src/main/java/es/uji/ei1027/skillsharing/controller/CollaborationController.java
@@ -3,6 +3,7 @@ package es.uji.ei1027.skillsharing.controller;
 import es.uji.ei1027.skillsharing.dao.CollaborationDao;
 import es.uji.ei1027.skillsharing.model.Collaboration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/collaboration")
 public class CollaborationController {
     private CollaborationDao collaborationDao;
 
